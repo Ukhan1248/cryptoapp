692ee577e5444856c553778baa46b193c622808f
Umair Khan <umair.khan.ece@gmail.com>
2022-01-10 11:03:00 -0600

    Commit 1 1/12/22

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
