3ddd518c18fc92221a3f9d58023deb72f584fab6
Umair Khan <umair.khan.ece@gmail.com>
2022-01-11 09:19:19 -0600

    Commit 1 1/11/22

--- src/App.js
+++ src/App.js
@@ -2,11 +2,45 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./components";
+import "./App.css";
+
 const App = () => {
   return (
     <div className="app">
-      <div className="navbar"></div>
-      <div className="main"></div>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
       <div className="footer"></div>
     </div>
   );
